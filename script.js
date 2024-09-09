f5156f65c79f1bb9595801bf14cc9dae84ccb38a
FabricioCamejo-code <fabry_18_rosariio@hotmail.com>
2024-09-09 18:58:43 -0300

    test_Ajax

---
+++ script.js
@@ -0,0 +1,12 @@
+function renderPage(url, data = {}, onLoad = null)
+{
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.addEventListener('load', () =>
+    {
+        const template = Handlebars.compile(xhr.response);
+        document.getElementById("view").innerHTML = template(data);
+        onLoad();
+    });
+}
